4e983f46efc7c8b6cd5412d0eea6c1e7576be98e
JoshEdgell <josh.0.edgell@gmail.com>
2017-08-18 19:17:20 -0400

    routes created (need to be checked)

--- controllers/dadjokes.js
+++ controllers/dadjokes.js
@@ -2,11 +2,46 @@ const express       = require('express');
 const router        = express.Router();
 const jokes         = require('../models/dadjokes.js');
 
-//route that (should) return the number of dad jokes we have
+//Count the number of dad jokes
 router.get('/count', (req,res)=>{
   jokes.count(function(error, count) {
     res.send(count.toString());
   });
+});
+
+//List of all dad jokes (have to check)
+router.get('/listall', (req,res)=>{
+  jokes.find({}, (error, jokes)=>{
+    res.json(jokes);
+  })
+});
+
+//Get single joke by id (have to check)
+router.get('/:id', (req,res)=>{
+  jokes.find({_id : req.params.id }, function(error,info){
+      res.send(info);
+  })
+});
+
+//New Joke (have to check)
+router.post('/', (req,res)=>{
+  jokes.create(req.body, (error, newJoke)=>{
+    res.json(newJoke);
+  })
+});
+
+//Edit joke (have to check)
+router.put('/:id', (req,res)=>{
+  jokes.findByIdAndUpdate(req.params.id, req.body, { new : true }, (error,updateJoke)=>{
+    res.json(updateJoke);
+  })
+});
+
+//Delete joke (have to check)
+router.delete(':/id', (req,res)=>{
+  jokes.findByIdAndRemove(req.params.id, (error, deletedJoke)=>{
+    res.json(deletedJoke);
+  })
 });
 
 
