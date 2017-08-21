cb38a67523d7224972b97161b077616db4b6e28b
JoshEdgell <josh.0.edgell@gmail.com>
2017-08-21 12:27:49 -0400

    route for registration written

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,42 @@
 const express       = require('express');
 const router        = express.Router();
-const user          = require('../models/user.js');
+const User          = require('../models/user.js');
 const bcrypt        = require('bcrypt');
 
 //Register New User
 router.get('/register', (req,res)=>{
   res.render('sessions/register.ejs', {
+    firstInput: '',
+    lastInput: '',
+    usernameInput: '',
+    passwordFail: false
+  });
+});
 
-  })
+router.post('/register', (req,res)=>{
+  if (req.body.password != req.body.password2){
+    res.render('sessions/register.ejs', {
+      passwordFail: true,
+      firstInput: req.body.firstName,
+      lastInput: req.body.lastName,
+      usernameInput: req.body.username
+    });
+  } else {
+    const password = req.body.password
+//Going with level 8 because I know this isn't top-secret enough to require level 10 security.
+const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+const userDbEntry = {};
+userDbEntry.firstName = req.body.firstName;
+userDbEntry.lastName = req.body.lastName;
+userDbEntry.username = req.body.username;
+userDbEntry.password = passwordHash;
+console.log(userDbEntry);
+User.create(userDbEntry, (err,user)=>{
+  req.session.username = user.username;
+  req.session.logged = true;
+  res.redirect('/');
+})
+  }
 })
 
 //Log out
